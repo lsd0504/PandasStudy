3af57f1f30095a98fd872057710edf84d4709204
lsd0504 <tpwnd98@naver.com>
2022-09-18 17:36:27 +0900

    Update compare_n.py
    새로 들어간 ORG 찾기

--- compare_n.py
+++ compare_n.py
@@ -1,3 +1,4 @@
+from ast import Or
 from turtle import width
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -18,25 +19,52 @@ mldl_AVG = pd.read_csv('./AVG.csv')
 # mldl_AVY = pd.read_csv('./AVY.csv')
 
 def comp_n(iwms,mldl):
+    date_change(mldl)
     Org = iwms.loc[2,'Org Code']
+    Date = iwms.loc[2,'Suggesstion Date']    
     cond_sugN = (iwms['AI Suggested Accuracy'] == 'N')&(iwms['Data Type'] != 'Transfer')
     iwms_sugN = iwms[cond_sugN].reset_index()
-    iwms_comp = iwms_sugN[['Part No', 'AI Suggested Locator', 'Actual Putaway Locator']]
-    df_mldl = mldl['PART_NO'].value_counts()
+    iwms_comp = iwms_sugN[['Part No', 'AI Suggested Locator']]
+    mldl_comp = mldl[['PART_NO', 'LOCATOR']]
     
-    # print(iwms_comp)
-    for i in range(len(iwms_comp)):
-        pNo_iwms = iwms_comp.loc[i,'Part No']
-        ailoc_iwms = iwms_comp.loc[i,'AI Suggested Locator']
-        actloc_iwms = iwms_comp.loc[i,'Actual Putaway Locator']
-        # print(pNo_iwms , df_mldl[pNo_iwms])
-        # print(df_mldl)
+    print('선택한 ORG : ' + Org)
+    drop_dup(mldl, Date)
+    
+    find_new_loc(iwms_comp, mldl_comp)
+    
+            
+def find_new_loc(iwms, mldl):
+    iwms_unique = iwms['AI Suggested Locator'].unique()
+    mldl_unique = mldl['LOCATOR'].unique()
+    
+    new_loc = list(set(iwms_unique).difference(mldl_unique))
+    print('AI 추천 locator가 데이터 상에서는 보이지만, 처음으로 들어간 경우 : ')
+    
+    if len(new_loc) < 1:
+        print('해당 없음')
+    else:
+        for i in new_loc:
+            same_loc = mldl['LOCATOR'] == i
+            print(same_loc)
+             
+    
+def drop_dup(mldl, Date):
+    iwms_month = Date[5:7]
+    iwms_day = Date[8:]
+          
+    for i in range(len(mldl)):
+        if iwms_month < mldl.loc[i,'DATE'][5:7]:
+            mldl = mldl.drop(i)
+        elif iwms_month == mldl.loc[i,'DATE'][5:7]:
+            if iwms_day <= mldl.loc[i,'DATE'][8:]:
+                mldl = mldl.drop(i)
+                    
+            
+def date_change(mldl):
+    for i in range(len(mldl)):
+        mldl.loc[i,'DATE'] = mldl.loc[i,'DATE'][:10]
         
-        if df_mldl[pNo_iwms] < 3:
-            print('AI 추천 locator가 데이터 상에서는 보이지만, 처음으로 들어간 경우')
-            print('Part No : ' + pNo_iwms + ', AI Sug Loc : ' + ailoc_iwms + ', Actual Loc : ' + actloc_iwms)
-        
-    
-    
+              
 # comp_n(iwms_AVB,mldl_AVB)
-comp_n(iwms_AVF,mldl_AVF)
+# comp_n(iwms_AVF,mldl_AVF)
+comp_n(iwms_AVG, mldl_AVG)
